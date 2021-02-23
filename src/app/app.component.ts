6ae6ad29ebd2443eafd68a48f68a0a2ffe1914a2
gaby205 <c14180055@john.petra.ac.id>
2021-02-23 18:16:59 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+  hasil = 0;
+  a=3;
+  b=4;
+  // item = {
+  //   nama : "jeruk", harga : 15000, satuan : "kg"
+  // }
+  item = [
+    {nama : "Jeruk", harga : 15000, satuan : "kg"},
+    {nama : "Apel", harga : 5000, satuan : "kg"}
+  ]
+
+  tampil : boolean = false
+  tulisanOPEN : String;
+
+  OPEN(){
+    this.tulisanOPEN ="TOKO DIBUKA";
+    alert("DIBUKA");
+  }
+
+  values ="";
+  onKey(event: any)
+  {
+    this.values += event.target.value + '|';
+  }
 }
